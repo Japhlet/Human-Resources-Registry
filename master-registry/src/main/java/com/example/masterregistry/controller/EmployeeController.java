ccf80d247815a5bfd9605ecdafe0f8c931f21166
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 14:12:07 +0300

    "Implemented logic - endpoint/api to retrieve all employees records from the database"

--- master-registry/src/main/java/com/example/masterregistry/controller/EmployeeController.java
+++ master-registry/src/main/java/com/example/masterregistry/controller/EmployeeController.java
@@ -1,8 +1,22 @@
 package com.example.masterregistry.controller;
 
+import com.example.masterregistry.entity.Employee;
+import com.example.masterregistry.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping(path = "/employees")
+    public List<Employee> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
 }
--- master-registry/src/main/java/com/example/masterregistry/service/EmployeeService.java
+++ master-registry/src/main/java/com/example/masterregistry/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package com.example.masterregistry.service;
 
+import com.example.masterregistry.entity.Employee;
+import com.example.masterregistry.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return  this.employeeRepository.findAll();
+    }
 }
