12b7e6b9a5d995e229ec52fec52c23d511b6f4e0
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 14:03:26 +0300

    "Added EmployeeRepository.java interface"

---
+++ master-registry/src/main/java/com/example/masterregistry/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.masterregistry.repository;
+
+import com.example.masterregistry.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
