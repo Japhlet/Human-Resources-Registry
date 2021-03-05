62eb5c668b704d1351bae73eabf02de8153b8e90
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 13:45:52 +0300

    first commit- bootstraped the Spring Boot Back-end Project

---
+++ master-registry/src/main/java/com/example/masterregistry/MasterRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterRegistryApplication.class, args);
+	}
+
+}
