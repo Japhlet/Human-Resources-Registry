ac2f8bdeb6a7e299b232d8ea6c2997f00f2eb0a1
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 15:58:40 +0300

    "Added routes for navigation"

--- master-registry-ui/src/app/app-routing.module.ts
+++ master-registry-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee/employee.component';
+import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'employees', component: EmployeeComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
