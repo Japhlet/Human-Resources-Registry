8636b9750a334ef086e37ddaf397142941aebbe6
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 15:50:47 +0300

    "Added Home and Employee Components"

--- master-registry-ui/src/app/app.module.ts
+++ master-registry-ui/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home/home.component';
+import { EmployeeComponent } from './employee/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
