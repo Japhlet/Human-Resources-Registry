071a268bd8e5a58ad33677aa4d33e8c428eabc6d
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 14:05:44 +0300

    "Added EmployeeController.java class"

---
+++ master-registry/src/main/java/com/example/masterregistry/controller/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.example.masterregistry.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
