c9718fb5739deb3d762ef603b8cf9bb62a184cd4
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-05 17:37:10 +0300

    "Added employee.model.ts and employee service"

---
+++ master-registry-ui/src/app/model/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    constructor(
+        id: number,
+        lastName: string,
+        firstName: string,
+        email: string,
+        department: string,
+        country: string
+    ) {
+
+    }
+}
