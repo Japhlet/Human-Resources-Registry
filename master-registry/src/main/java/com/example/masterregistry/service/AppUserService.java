5f4ce440647906140fd9c9659bd2dc7fae6e364a
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-06 12:38:02 +0300

    "Added AppUserService.java class for registering system users"

---
+++ master-registry/src/main/java/com/example/masterregistry/service/AppUserService.java
@@ -0,0 +1,6 @@
+package com.example.masterregistry.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class AppUserService implements UserDetailsService {
+}
