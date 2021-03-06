89f08daa7135b72ed3ef01029ae313050581452d
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-06 14:01:37 +0300

    "Added App Users navigation link"

--- master-registry-ui/src/app/app-routing.module.ts
+++ master-registry-ui/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee/employee.component';
 import { HomeComponent } from './home/home/home.component';
+import { UserregistrationComponent } from './userregistration/userregistration/userregistration.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'employees', component: EmployeeComponent },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UserregistrationComponent },
 ];
 
 @NgModule({
