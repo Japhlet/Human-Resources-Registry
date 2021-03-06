5108332d1fe649daea2ed8e78c371a2789cfc8a3
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-06 13:41:37 +0300

    "Added userregistration and userlogin components"

--- master-registry-ui/src/app/app.module.ts
+++ master-registry-ui/src/app/app.module.ts
@@ -9,12 +9,16 @@ import { EmployeeComponent } from './employee/employee/employee.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeService } from './service/employee.service';
 import { FormBuilder, FormControl, FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { UserregistrationComponent } from './userregistration/userregistration/userregistration.component';
+import { UserloginComponent } from './userlogin/userlogin/userlogin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    UserregistrationComponent,
+    UserloginComponent
   ],
   imports: [
     BrowserModule,
