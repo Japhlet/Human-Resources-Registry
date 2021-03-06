2d6b58f079f2f61be73269378cfc4cc12488b037
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-06 11:49:56 +0300

    "Implemeted logic to validate email"

--- master-registry/src/main/java/com/example/masterregistry/service/EmployeeService.java
+++ master-registry/src/main/java/com/example/masterregistry/service/EmployeeService.java
@@ -14,17 +14,33 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private EmailValidator emailValidator;
+
     public List<Employee> getAllEmployees() {
         return  this.employeeRepository.findAll();
     }
 
     public void addEmployee(Employee employee) {
+        boolean isValidEmail = emailValidator.test(employee.getEmail());
+
+        if(!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+
         this.employeeRepository.save(employee);
     }
 
     public void updateEmployee(Employee employee) {
         Employee employeeToUpdate = employeeRepository.findById(employee.getId())
                 .orElseThrow(() -> new EmployeeNotFoundException("Employee with id "+employee.getId()+" not found"));
+
+
+        boolean isValidEmail = emailValidator.test(employee.getEmail());
+
+        if(!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
 
         employeeToUpdate.setLastName(employee.getLastName());
         employeeToUpdate.setFirstName(employee.getFirstName());
