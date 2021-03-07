88b3faed7aa25992da8af11275068f3d39f024db
Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2021-03-07 15:59:49 +0300

    "Updated UI to Edit, Delete and Show details of App User"

--- master-registry-ui/src/app/userregistration/appuserslist/appuserslist.component.ts
+++ master-registry-ui/src/app/userregistration/appuserslist/appuserslist.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppuserslistService } from 'src/app/appuserslist.service';
 import { AppUser } from 'src/app/model/userregistration.model';
@@ -17,38 +17,48 @@ export class AppuserslistComponent implements OnInit {
   closeResult: string;
   userCreatedSuccessfulMessage = '';
   userCreatedNotSuccessfulMessage = '';
+  editForm: FormGroup;
+  appUserToDeleteId: number;
 
   constructor(
     private appUserslistService: AppuserslistService,
     private userRegistrationService: UserregistrationService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.getAllAppUsers();
+
+    this.editForm = this.formBuilder.group({
+      id: [''],
+      lastName: [''],
+      firstName: [''],
+      email: ['']
+    });
   }
 
-  public getAllAppUsers(): void {    
+  public getAllAppUsers(): void {
     this.appUserslistService.getAllAppUsers().subscribe(
-      (response: AppUser[]) => {        
+      (response: AppUser[]) => {
         this.appUsers = response;
       },
-      (error: HttpErrorResponse) => {        
+      (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
   }
 
   openRegisterAppUserModal(registerAppUserModal) {
-    this.modalService.open(registerAppUserModal, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(registerAppUserModal, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
-  private getDismissReason(reason: any) : string {
-    if(reason === ModalDismissReasons.ESC) {
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
@@ -57,16 +67,68 @@ export class AppuserslistComponent implements OnInit {
     }
   }
 
-  onSubmit(registerAppUserForm: NgForm) {    
+  onSubmit(registerAppUserForm: NgForm) {
     this.userRegistrationService.registerAppUser(registerAppUserForm.value).subscribe(
       data => {
         this.ngOnInit(); //Reload the table
         this.userCreatedSuccessfulMessage = "User added successfully.";
       },
-      error => { console.log("An exception occurred");
-      this.userCreatedNotSuccessfulMessage = "Bad credentials, please enter valid email and password";
-    }   
+      error => {
+        console.log("An exception occurred");
+        this.userCreatedNotSuccessfulMessage = "Bad credentials, please enter valid email and password";
+      }
     );
     this.modalService.dismissAll() //Dismiss the modal
-  } 
+  }
+
+  openAppUserDetailsModal(appUserDetailsModal, appUser: AppUser) {
+    this.modalService.open(appUserDetailsModal, {
+      centered: true,
+      backdrop: 'static',
+      size: 'lg'
+    });
+    document.getElementById('dtlastName').setAttribute('value', appUser.lastName);
+    document.getElementById('dtfirstName').setAttribute('value', appUser.firstName);
+    document.getElementById('dtemail').setAttribute('value', appUser.email);
+  }
+
+  openEditAppUserDetailsModal(editAppUserDetailsModal, appUser: AppUser) {
+    this.modalService.open(editAppUserDetailsModal, {
+      backdrop: 'static',
+      size: 'lg'
+    });
+    this.editForm.patchValue({
+      id: appUser.id,
+      lastName: appUser.lastName,
+      firstName: appUser.firstName,
+      email: appUser.email
+    });
+  }
+
+  //onSave() {
+  //this.userRegistrationService.updateAppUser(this.editForm.value)
+  //.subscribe((results) => {
+  // this.ngOnInit();
+  // this.modalService.dismissAll();
+  // });
+  // }
+
+  openDeleteAppUserModal(deleteAppUserModal, appUser: AppUser) {
+    this.appUserToDeleteId = appUser.id;
+    this.modalService.open(deleteAppUserModal, {
+      backdrop: 'static',
+      size: 'lg'
+    });
+  }
+
+  //onDelete() {
+    //this.userRegistrationService.deleteAppUser(this.appUserToDeleteId)
+     // .subscribe((results) => {
+      //  this.ngOnInit();
+      //  this.modalService.dismissAll();
+     // });
+  //}
+
 }
+
+
